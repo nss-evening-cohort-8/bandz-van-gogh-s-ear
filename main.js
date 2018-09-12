5a4f9e5300aeebe103995421185edce047113af4
Colin White <clwhite814@gmail.com>
2018-09-11 21:24:33 -0500

    WIP

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ const printToDom = (divId, stringToPrint) => {
 };
 
 printToDom('band-name', 'Print to Dom function works');
+
+// TOUR PAGE OBJECT 
+
+let tourDates = {
+    date: ["September 11 2018", "September 12 2018", "September 13 2018"],
+    location: ["Los Angelos", "Nashville", "Atlanta"],
+    venue: ["Staples Center", "Georgia Dome", "Mercedes Benz Stadium" ],
+    tickets: ["SOLD OUT", "Staples Center", "Staples Center"]
+};
+
+const tourStringBuilder = () => {
+    let theString = ''; 
+    for (let i = 0; i < tourDates.length; i++) {
+        theString += `<div class="tour-styling">`
+        theString += `<h2>${tourDates.[i]date</h2>`;
+        theString += `<h2>${tourDates.[i]location}</h2>`;
+        theString += `<h2>${tourDates.[i]venue}</h2>`;
+        theString += `<h2>${tourDates.[i]tickets}</h2>`;
+        theString += `</div>`;
+    }
+    printToDom("tour-dates", theString);
+};
+
+tourStringBuilder();
