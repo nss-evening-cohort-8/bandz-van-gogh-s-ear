afec36bc9c3d9b0b9aaf347b568e4ccb7a4c5c12
WaysideTester <dylanmurray2119@gmail.com>
2018-09-13 18:38:54 -0500

    restructured data

--- main.js
+++ main.js
@@ -6,14 +6,11 @@ const printToDom = (divId, stringToPrint) => {
 };
 
 // Start discography object
-const discography = {
-    art: ['https://images-na.ssl-images-amazon.com/images/I/91WkS-FlHKL._SX522_.jpg', 'https://yt3.ggpht.com/a-/AN66SAwX5j4kWGCl1EJ6pqjhb79hSs8V7gTsNIX77A=s288-mo-c-c0xffffffff-rj-k-no', 'https://www.getdigital.eu/web/getdigital/gfx/products/__generated__resized/1100x1100/helloworld_poster.jpg', 'https://discuss.as3lang.org/uploads/default/original/1X/9e1b0c4c4f34af307ff87e135b9faff9bb218935.png'],
-    albums: ['For Richard the Plastic Dinosaur, Wherever He May Be', 'Infinite Loop', 'Int Main ()', 'Push 2 Master'],
-    songs: 
-    [
-        {  
-            album: 'For Richard the Plastic Dinosaur, Wherever He May Be', 
-            song:[
+const discography = [
+    {
+        art: 'https://images-na.ssl-images-amazon.com/images/I/91WkS-FlHKL._SX522_.jpg',
+        albums: 'For Richard the Plastic Dinosaur, Wherever He May Be',
+        songs: [
                 'Intro',
                 'The Decipherment Of Linear B',
                 'For Richard The Plastic Dinosaur, Wherever He May Be',
@@ -25,10 +22,11 @@ const discography = {
                 'All The Things You Know',
                 'No Exit Condition'
             ]
-        },
-        {
-            album: 'Infinite Loop',
-            song:[
+    },
+    {
+        art: 'https://yt3.ggpht.com/a-/AN66SAwX5j4kWGCl1EJ6pqjhb79hSs8V7gTsNIX77A=s288-mo-c-c0xffffffff-rj-k-no',
+        albums: 'Infinite Loop',
+        songs:[
                 'Get Me Off This Ride',
                 'Alt + Ctrl + Delete',
                 'No End In Sight',
@@ -39,40 +37,42 @@ const discography = {
                 'Reboot',
                 'For Loop Jam - Live version',
                 'The Void - Live version'
-            ]
-        },
-        {
-            album: 'Int Main ()',
-            song: [
-                'Bear Stix',
-                'Broadway Women',
-                'Midnight at Steak n Shake',
-                'Street Pharmacist',
-                'Windows 95',
-                'Soccer Circus',
-                'Percy Priest Romance',
-                'Skin Rash Jam',
-                'Illusions of Sanity',
-                'Hymn for the Lost Generation'
-            ]
-        },
-        {
-            album: 'Push 2 Master',
-            song: [
-                'TI-86 4 Lyf',
-                'Give Me Ramen or Give Me Death',
-                'Cipher',
-                'Splinters',
-                'Panic!',
-                'Smashed To Pieces',
-                '11 PM on a Friday Night',
-                'Drum solo',
-                'TI-2000',
-                'Coda'
-            ]
-        }
-    ]
-};
+        ]
+    },
+    {
+        art: 'https://www.getdigital.eu/web/getdigital/gfx/products/__generated__resized/1100x1100/helloworld_poster.jpg',
+        albums: 'Int Main ()',
+        songs: [
+            'Bear Stix',
+            'Broadway Women',
+            'Midnight at Steak n Shake',
+            'Street Pharmacist',
+            'Windows 95',
+            'Soccer Circus',
+            'Percy Priest Romance',
+            'Skin Rash Jam',
+            'Illusions of Sanity',
+            'Hymn for the Lost Generation'
+        ]
+    },
+    {
+        art: 'https://discuss.as3lang.org/uploads/default/original/1X/9e1b0c4c4f34af307ff87e135b9faff9bb218935.png',
+        albums: 'Push 2 Master',
+        songs: [
+            'TI-86 4 Lyf',
+            'Give Me Ramen or Give Me Death',
+            'Cipher',
+            'Splinters',
+            'Panic!',
+            'Smashed To Pieces',
+            '11 PM on a Friday Night',
+            'Drum solo',
+            'TI-2000',
+            'Coda'
+        ]
+    }
+]
+    
+    
 
-console.log(discography.songs[2].song[3]);
-
+console.log(discography[1].songs[4]);
