5a6aaf1ef3daf0429d3de6e77abb01b7be5d4e1d
WaysideTester <dylanmurray2119@gmail.com>
2018-09-16 21:18:38 -0500

    ok

--- main.js
+++ main.js
@@ -363,8 +363,8 @@ const newFunct = (id) => {
     console.log('functioning');
 }
 
-let olSelector = [document.getElementById('The Decipherment Of Linear B'), document.getElementById('Alt + Ctrl + Delete'), document.getElementById('Broadway Women'), document.getElementById('Give Me Ramen or Give Me Death')];
-let buttonSelector = [olSelector[0].parentNode.childNodes[5],olSelector[1].parentNode.childNodes[5],olSelector[2].parentNode.childNodes[5],olSelector[3].parentNode.childNodes[5]];
+const olSelector = [document.getElementById('The Decipherment Of Linear B'), document.getElementById('Alt + Ctrl + Delete'), document.getElementById('Broadway Women'), document.getElementById('Give Me Ramen or Give Me Death')];
+const buttonSelector = [olSelector[0].parentNode.childNodes[5],olSelector[1].parentNode.childNodes[5],olSelector[2].parentNode.childNodes[5],olSelector[3].parentNode.childNodes[5]];
 buttonSelector[0].addEventListener('click', function(){
     newFunct(olSelector[0]);
 })
